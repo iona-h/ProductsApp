ecf32b0b2f05b2f391fc3cb273ee46c97a2e3c7f
Iona Hodgetts <iona.hodgetts@gmail.com>
2022-11-28 01:59:53 +0000

    Add project files.

---
+++ ProductsApp/Controllers/ProductController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsApp.Context;
+using ProductsApp.Models;
+using System.Net;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductContext db = new ProductContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        // GET: Product/Details/5
+        //? on int allows input to be null or int
+        public ActionResult Details(int? id)
+        {
+            //check if id is null, if so say it was a bad request
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //find product related to id
+            Product product = db.Products.Find(id);
+            //if no product for that id, say product not found
+            if (product == null)
+                return HttpNotFound();
+            //otherwise
+            return View(product);
+        }
+
+        // GET: Product/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? ID)
+        {
+            if (ID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Product product = db.Products.Find(ID);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? id, Product prod)
+        {
+            try
+            {
+                Product product = new Product();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+
+                    product = db.Products.Find(id);
+
+                    if (product == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    db.Products.Remove(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+
+                return View(product);
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
